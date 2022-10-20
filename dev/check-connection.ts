3c8792fcc0cad1e8aba43ef3876421cadf4d69fb
Anton Karytka <a.karytka@gmail.com>
2022-10-20 16:44:50 +0200

    Fix "yarn start-postgres-latest".

--- dev/check-connection.ts
+++ dev/check-connection.ts
@@ -3,6 +3,11 @@ import { createSequelizeInstance } from '../test/support';
 const sequelize = createSequelizeInstance();
 
 (async () => {
+  // Temporary solution. Otherwise will get "Connection terminated unexpectedly".
+  await new Promise(resolve => {
+    setTimeout(resolve, 10_000);
+  });
+
   await sequelize.authenticate();
   await sequelize.close();
 
